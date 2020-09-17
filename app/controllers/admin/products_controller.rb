d017ce92e1ad1cf9eb015c811cc8df330c07a7f9
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-09-17 01:33:38 +0000

    Fixed missing admin security

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN_USER_ID"], password: ENV["ADMIN_USER_PASSWORD"]
+  
   def index
     @products = Product.order(id: :desc).all
   end
